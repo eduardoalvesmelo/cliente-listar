c73e36e5eee88bd3445149712a9c8cffa4c02983
Eduardo Alves de Melo <eduardoalvesmelo.2018@gmail.com>
2023-11-21 19:56:37 -0400

    properties, Cliente e ORM

---
+++ src/main/java/com/cliente/listar/clientelistar/Entities/Cliente.java
@@ -0,0 +1,62 @@
+package com.cliente.listar.clientelistar.Entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String cpf;
+    private String nome;
+
+    public Cliente() { }
+
+    public Cliente(int id, String cpf, String nome) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
