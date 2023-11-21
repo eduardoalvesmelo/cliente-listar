d9a4a3dacef5838262847e91dac4d1499f500225
Eduardo Alves de Melo <eduardoalvesmelo.2018@gmail.com>
2023-11-21 19:12:59 -0400

    project created

---
+++ src/main/java/com/cliente/listar/clientelistar/ClienteListarApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.listar.clientelistar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteListarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteListarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cliente/listar/clientelistar/ClienteListarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.listar.clientelistar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteListarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
